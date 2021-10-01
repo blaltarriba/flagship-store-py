82d5e6b4cb1e65da92333315a9c012cb79600cfc
Borja Lopez Altarriba <borja.lopez83@gmail.com>
2021-10-01 18:39:51 +0200

    Dockerize project

--- src/main.py
+++ src/main.py
@@ -44,5 +44,5 @@ def delete_user(user_id: int):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8005)
+    uvicorn.run(app, host='0.0.0.0', port=3080)
     print("running")
